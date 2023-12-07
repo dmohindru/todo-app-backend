ef3d53b5966de87af11e1aff406e45b230ef5f01
Dhruv Mohindru <dmohindru@gmail.com>
2023-12-08 06:59:03 +1100

    Initial commit for this repository

---
+++ src/main/java/dev/dmohindru/todoappbackend/TodoAppBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.dmohindru.todoappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/dmohindru/todoappbackend/TodoAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dmohindru.todoappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
